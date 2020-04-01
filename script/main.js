b789849a704ce03f2a7e628dcf04d671eba56923
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-01 17:06:14 +0200

    solo console

--- script/main.js
+++ script/main.js
@@ -30,3 +30,23 @@ for (n = 1; n <= 100; n++) {
 
 // Stampo il risultato
 listaNumeri.innerHTML = items;
+
+// SOLO CONSOLE
+/*
+for (n = 1; n <= 100; n++) {
+  print = n;
+
+  // Pongo le condizioni
+  if ((n % 3 == 0) && (n % 5 == 0)) {
+    print = 'FizzBuzz';
+  }
+  else if (n % 3 == 0) {
+    print = 'Fizz';
+  }
+  else if (n % 5 == 0) {
+    print = 'Buzz';
+  }
+
+  console.log(print);
+};
+*/
